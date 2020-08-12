f6fd36c0051f5ad894f82878b9bf2514a6fbed68
BonfireScratch <sigsaleum@gmail.com>
2020-08-12 09:14:23 +0300

    Updates

---
+++ precache-manifest.9cfc74b3f702775565e4feaa6e9838db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb206f16476aa976902ec2bcd76b463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a8b4072290d8da794a",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e491c06450ab7a26f6ef",
+    "url": "/static/css/main.4344d81b.chunk.css"
+  },
+  {
+    "revision": "64a8b4072290d8da794a",
+    "url": "/static/js/2.182f82d4.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/static/js/2.182f82d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e491c06450ab7a26f6ef",
+    "url": "/static/js/main.aa8cf823.chunk.js"
+  },
+  {
+    "revision": "e340dc865d49e5f98681",
+    "url": "/static/js/runtime-main.85ee23ec.js"
+  },
+  {
+    "revision": "bd427603ee5480ebc7c877d41487d12a",
+    "url": "/static/media/dual-screen-1745705_1280.bd427603.png"
+  }
+]);
